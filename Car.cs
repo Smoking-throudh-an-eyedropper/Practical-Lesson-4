9137cb3c67604e682f66ee817c334ba2e806919b
Smoking-throudh-an-eyedropper <menshagina.a@yandex.ru>
2023-03-30 09:20:58 +0700

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+
+namespace Practical_Lesson_4
+{
+    internal class Car
+    {
+        private string Stamp;
+        private int NumberOfCylinders;
+        private int LoadCapacity;
+        public Car(string stamp, int numberOfCylinders, int power)
+        {
+            if (Stamp == null)
+                throw new NullReferenceException("Stamp");
+            Stamp = stamp;
+            NumberOfCylinders = numberOfCylinders;
+            LoadCapacity = power;
+        }
+        void Read()
+        {
+            Console.WriteLine("Укажите торговую марку:");
+            Stamp = Convert.ToString(Console.Read());
+            Console.WriteLine("Укажите количество цилиндров:");
+            NumberOfCylinders = Convert.ToInt32(Console.Read());
+            Console.WriteLine("Укажите мощность:");
+            LoadCapacity = Convert.ToInt32(Console.Read());
+            Console.WriteLine("");
+        }
+        void Display()
+        {
+            Console.WriteLine("Automobile");
+            Console.WriteLine($"Stamp:{Stamp}");
+            Console.WriteLine($"NumberOfCylinders:{NumberOfCylinders}");
+            Console.WriteLine($"Power:{LoadCapacity}");
+        }
+        public int ReassignmentPower(Car car)
+        {
+            if (car == null)
+                throw new NullReferenceException("car");
+            Console.WriteLine($"Мощность автомобиля: {car.LoadCapacity}");
+            int NewPower = Convert.ToInt32(Console.ReadLine());
+            return car.LoadCapacity = NewPower;
+            Console.WriteLine($"Мощность автомобиля: {car.LoadCapacity}");
+        }
+    }
+}
---
+++ Lorry.cs
@@ -0,0 +1,42 @@
+﻿using Practical_Lesson_4;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Lesson__4
+{
+    internal class Lorry: Car
+    {
+        private int LoadCapacity;
+        public Lorry(string stamp, int numberOfCylinders, int power, int loadCapacity) :
+           base(stamp, numberOfCylinders, power)
+        {
+            if (stamp == null)
+                throw new NullReferenceException("stamp");
+            LoadCapacity = loadCapacity;
+        }
+        void Read()
+        {
+            Console.WriteLine("Укажите грузоподъёмность:");
+            LoadCapacity = Convert.ToInt32(Console.Read());
+            Console.WriteLine("");
+        }
+        void Display()
+        {
+            Console.WriteLine("Lorry");
+            Console.WriteLine($"LoadCapacity:{LoadCapacity}");
+        }
+        public int ReassignmentLoadCapacity(Lorry lorry)
+        {
+            if (lorry == null)
+                throw new NullReferenceException("lorry");
+            Console.WriteLine($"Грузоподъёмность: {lorry.LoadCapacity}");
+            int NewLoadCapacity = Convert.ToInt32(Console.ReadLine());
+            return lorry.LoadCapacity = NewLoadCapacity;
+            Console.WriteLine($"Грузоподъёмность: {lorry.LoadCapacity}");
+        }
+    }
+}
